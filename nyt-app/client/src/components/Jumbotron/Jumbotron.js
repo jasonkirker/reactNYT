535f488d01a1772b92d2b350240d417847342fd5
Vance <jasonkirker@users.noreply.github.com>
2018-10-29 09:27:08 -0500

    set up structure of app

---
+++ nyt-app/client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = ({ children }) => (
+  <div className="jumbotron mt-2">
+    {children}
+  </div>
+);
+
+export default Jumbotron;
